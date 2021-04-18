5f06ace7e979adca6a979c9bd32e1f0669374c02
annehe9 <afhe@andrew.cmu.edu>
2021-04-18 11:40:03 -0400

    it works but explodes at bottom

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 #include <eigen3/Eigen/Dense>
@@ -16,181 +19,251 @@ using namespace Eigen;
 #define M_PI 3.14159265358979323846
 #endif
 
-// "Particle-Based Fluid Simulation for Interactive Applications"
-// solver parameters
-const static Vector2f G(0.f, 12000 * -9.8f); // external (gravitational) forces
-const static float REST_DENS = 1000.f;		 // rest density
-const static float GAS_CONST = 2000.f;		 // const for equation of state
-const static float H = 16.f;				 // kernel radius
-const static float HSQ = H * H;				 // radius^2 for optimization
-const static float MASS = 65.f;				 // assume all particles have the same mass
-const static float VISC = 250.f;			 // viscosity constant
-const static float DT = 0.0001f;			 // integration timestep
+// References:
+// https://github.com/yuanming-hu/taichi_mpm/blob/master/mls-mpm88-explained.cpp
+// https://lucasschuermann.com/writing/implementing-sph-in-2d for visualization
 
-// smoothing kernels defined in Müller and their gradients
-const static float POLY6 = 315.f / (65.f * M_PI * pow(H, 9.f));
-const static float SPIKY_GRAD = -45.f / (M_PI * pow(H, 6.f));
-const static float VISC_LAP = 45.f / (M_PI * pow(H, 6.f));
-
-// simulation parameters
-const static float EPS = H; // boundary epsilon
-const static float BOUND_DAMPING = -0.5f;
-
-// particle data structure
-// stores position, velocity, and force for integration
-// stores density (rho) and pressure values for SPH
+// Particle representation
 struct Particle
 {
-	Particle(float _x, float _y, float m) : x(_x, _y), v(0.f, 0.f), mass(m) {}
-	Vector2f x, v;
-	float mass;
+	Particle(double _x, double _y) : x(_x, _y), v(0.0, 0.0), F(Matrix2d::Identity()), C(Matrix2d::Zero()), Jp(1.0) {}
+	Vector2d x, v; //position and velocity
+	Matrix2d F, C; //deformation gradient, APIC momentum
+	double Jp; //determinant of deformation gradient, which is volume
 };
 
+// Grid representation
+/*
 struct Cell
 {
 	Cell() : v(0.f, 0.f), mass(0.f) {}
 	Vector2f v;
-	float mass;
+	double mass;
 
 };
+*/
 
-// solver data
-static vector<Particle> particles;
-static vector<Cell> grid;
-
-// interaction
+// Granularity
 const static int MAX_PARTICLES = 2500;
-const static int DAM_PARTICLES = 500;
-const static int BLOCK_PARTICLES = 250;
-const static int NUM_PARTICLES = 500;
-const static int GRID_RES = 64;
-const static int NUM_CELLS = GRID_RES * GRID_RES;
+const static int BLOCK_PARTICLES = 500;		// number of particles added in a block
+int NUM_PARTICLES = 0;					// keeps track of current number of particles
+const static int GRID_RES = 80;				// grid dim of one side
+const static int NUM_CELLS = GRID_RES * GRID_RES;	// number of cells in the grid
+const static double DT = 0.00001;			// integration timestep
+const static double DX = 1.0 / GRID_RES;
+const static double INV_DX = 1.0 / DX;
 
-// rendering projection parameters
+// Data structures
+static vector<Particle> particles;
+// Vector3: [velocity_x, velocity_y, mass]
+static Vector3d grid[GRID_RES][GRID_RES];
+//static Cell grid[GRID_RES][GRID_RES];
+
+// Simulation params
+const static double MASS = 1.0;					// mass of one particle
+const static double VOL = 1.0;					// volume of one particle
+const static double HARD = 10.0;					// snow hardening factor
+const static double E = 10000;				// Young's Modulus, resistance to fracture
+const static double NU = 0.2;					// Poisson ratio
+
+// Initial Lame params
+const static double MU_0 = E / (2 * (1 + NU));
+const static double LAMBDA_0 = (E * NU) / ((1 + NU) * (1 - 2 * NU));
+
+// Render params
 const static int WINDOW_WIDTH = 800;
 const static int WINDOW_HEIGHT = 600;
-const static double VIEW_WIDTH = 1.5 * 800.f;
-const static double VIEW_HEIGHT = 1.5 * 600.f;
+const static double VIEW_WIDTH = 1.5 * 800;
+const static double VIEW_HEIGHT = 1.5 * 600;
+
+// yay add more particles randomly in a square
+void addParticles(double xcenter, double ycenter)
+{
+	for (int i = 0; i < BLOCK_PARTICLES; i++) {
+		particles.push_back(Particle((((double)rand() / (double)RAND_MAX) * 2 - 1) * 0.08 + xcenter, (((double)rand() / (double)RAND_MAX) * 2 - 1) * 0.08 + ycenter));
+	}
+	NUM_PARTICLES += BLOCK_PARTICLES;
+}
 
 void InitMPM(void)
 {
-	cout << "initializing mpm with " << DAM_PARTICLES << " particles" << endl;
-	for (float y = EPS; y < VIEW_HEIGHT - EPS * 2.f; y += H) {
-		for (float x = VIEW_WIDTH / 4; x <= VIEW_WIDTH / 2; x += H) {
-			if (particles.size() < DAM_PARTICLES)
-			{
-				float jitter = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-				particles.push_back(Particle(x + jitter, y, 1.f));
-			}
-		}
-	}
-		
-	for (int i = 0; i < NUM_CELLS; i++) {
-		grid.push_back(Cell());
-	}
-}
-
-void Integrate(void)
-{
-	for (auto& p : particles)
-	{
-		// forward Euler integration
-		p.v += DT * p.f / p.rho;
-		p.x += DT * p.v;
-
-		// enforce boundary conditions
-		if (p.x(0) - EPS < 0.0f)
-		{
-			p.v(0) *= BOUND_DAMPING;
-			p.x(0) = EPS;
-		}
-		if (p.x(0) + EPS > VIEW_WIDTH)
-		{
-			p.v(0) *= BOUND_DAMPING;
-			p.x(0) = VIEW_WIDTH - EPS;
-		}
-		if (p.x(1) - EPS < 0.0f)
-		{
-			p.v(1) *= BOUND_DAMPING;
-			p.x(1) = EPS;
-		}
-		if (p.x(1) + EPS > VIEW_HEIGHT)
-		{
-			p.v(1) *= BOUND_DAMPING;
-			p.x(1) = VIEW_HEIGHT - EPS;
-		}
-	}
-}
-
-void ComputeDensityPressure(void)
-{
-	for (auto& pi : particles)
-	{
-		pi.rho = 0.f;
-		for (auto& pj : particles)
-		{
-			Vector2f rij = pj.x - pi.x;
-			float r2 = rij.squaredNorm();
-
-			if (r2 < HSQ)
-			{
-				// this computation is symmetric
-				pi.rho += MASS * POLY6 * pow(HSQ - r2, 3.f);
-			}
-		}
-		pi.p = GAS_CONST * (pi.rho - REST_DENS);
-	}
-}
-
-void ComputeForces(void)
-{
-	for (auto& pi : particles)
-	{
-		Vector2f fpress(0.f, 0.f);
-		Vector2f fvisc(0.f, 0.f);
-		for (auto& pj : particles)
-		{
-			if (&pi == &pj)
-				continue;
-
-			Vector2f rij = pj.x - pi.x;
-			float r = rij.norm();
-
-			if (r < H)
-			{
-				// compute pressure force contribution
-				fpress += -rij.normalized() * MASS * (pi.p + pj.p) / (2.f * pj.rho) * SPIKY_GRAD * pow(H - r, 2.f);
-				// compute viscosity force contribution
-				fvisc += VISC * MASS * (pj.v - pi.v) / pj.rho * VISC_LAP * (H - r);
-			}
-		}
-		Vector2f fgrav = G * pi.rho;
-		pi.f = fpress + fvisc + fgrav;
-	}
-}
-
-void ClearGrid(void)
-{
-	for (Cell c : grid) {
-		c.mass = 0;
-		c.v = Vector2f::Zero();
-	}
+	cout << "initializing mpm with " << BLOCK_PARTICLES << " particles" << endl;
+	addParticles(0.55, 0.45);
+	addParticles(0.45, 0.65);
+	addParticles(0.55, 0.85);
 }
 
 void P2G(void)
 {
-	for (Particle p : particles) {
-		int cell_idx = p.x(0) + (p.x(1) * VIEW_WIDTH);
+	memset(grid, 0, sizeof(grid));
+	for (Particle &p : particles) {
+		Vector2i base_coord = (p.x * INV_DX - Vector2d(0.5, 0.5)).cast<int>();
+		Vector2d fx = p.x * INV_DX - base_coord.cast<double>();
 
+		// Quadratic kernels [https://www.seas.upenn.edu/~cffjiang/research/mpmcourse/mpmcourse.pdf Eqn. 123, with x=fx, fx-1,fx-2]
+		Vector2d onehalf(1.5, 1.5); //have to make these so eigen doesn't give me errors
+		Vector2d one(1.0, 1.0);
+		Vector2d half(0.5, 0.5);
+		Vector2d threequarters(0.75, 0.75);
+		Vector2d tmpa = onehalf - fx;
+		Vector2d tmpb = fx - one;
+		Vector2d tmpc = fx - half;
+		Vector2d w[3] = {
+		  0.5 * (tmpa.cwiseProduct(tmpa)),
+		  threequarters - (tmpb.cwiseProduct(tmpb)),
+		  0.5 * (tmpc.cwiseProduct(tmpc))
+		};
+
+		// Compute current Lamé parameters [https://www.seas.upenn.edu/~cffjiang/research/mpmcourse/mpmcourse.pdf Eqn. 87]
+		double e = exp(HARD * (1.0 - p.Jp));
+		double mu = MU_0 * e;
+		double lambda = LAMBDA_0 * e;
+
+		// Current volume
+		double J = p.F.determinant();
+
+		// Polar decomposition for fixed corotated model
+		JacobiSVD<Matrix2d> svd(p.F, ComputeFullU | ComputeFullV);
+		Matrix2d r = svd.matrixU() * svd.matrixV().transpose();
+		//Matrix2d s = svd.matrixV() * svd.singularValues().asDiagonal() * svd.matrixV().transpose();
+
+		// [http://mpm.graphics Paragraph after Eqn. 176]
+		double Dinv = 4 * INV_DX * INV_DX;
+		// [http://mpm.graphics Eqn. 52]
+		Matrix2d PF_0 = (2 * mu) * (p.F - r) * p.F.transpose();
+		double pf1tmp= (lambda * (J - 1) * J);
+		Matrix2d PF_1;
+		PF_1 << pf1tmp, pf1tmp, 
+				pf1tmp, pf1tmp;
+		Matrix2d PF = PF_0 + PF_1;
+
+		// Cauchy stress times dt and inv_dx
+		Matrix2d stress = -(DT * VOL) * (Dinv * PF);
+
+		// Fused APIC momentum + MLS-MPM stress contribution
+		// See https://yzhu.io/publication/mpmmls2018siggraph/paper.pdf
+		// Eqn 29
+		Matrix2d affine = stress + MASS * p.C;
+
+		// P2G
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				Vector2d dpos = (Vector2d(i, j) - fx) * DX;
+				// Translational momentum
+				Vector3d mass_x_velocity(p.v.x() * MASS, p.v.y() * MASS, MASS);
+				Vector2d tmp = affine * dpos;
+				grid[base_coord.x() + i][base_coord.y() + j] += (
+					w[i].x() * w[j].y() * (mass_x_velocity + Vector3d(tmp.x(), tmp.y(), 0))
+					);
+			}
+		}
+	}
+}
+
+void UpdateGridVelocity(void) {
+	// For all grid nodes
+	for (int i = 0; i <= GRID_RES; i++) {
+		for (int j = 0; j <= GRID_RES; j++) {
+			auto& g = grid[i][j];
+			// No need for epsilon here
+			if (g[2] > 0) {
+				// Normalize by mass
+				g /= g[2];
+				// Gravity
+				g += DT * Vector3d(0, -200, 0);
+
+				// boundary thickness
+				double boundary = 0.05;
+				// Node coordinates
+				double x = (double)i / GRID_RES;
+				double y = (double)j / GRID_RES;
+
+				// Sticky boundary
+				if (x < boundary || x > 1 - boundary || y > 1 - boundary) {
+					g = Vector3d::Zero();
+				}
+				// Separate boundary
+				if (y < boundary) {
+					g[1] = std::max(0.0, (double)g[1]);
+				}
+			}
+		}
+	}
+}
+
+void G2P(void)
+{
+	for (Particle &p : particles) {
+		// element-wise floor
+		Vector2i base_coord = (p.x * INV_DX - Vector2d(0.5, 0.5)).cast<int>();
+		Vector2d fx = p.x * INV_DX - base_coord.cast<double>();
+
+		// Quadratic kernels [https://www.seas.upenn.edu/~cffjiang/research/mpmcourse/mpmcourse.pdf Eqn. 123, with x=fx, fx-1,fx-2]
+		Vector2d onehalf(1.5, 1.5); //have to make these so eigen doesn't give me errors
+		Vector2d one(1.0, 1.0);
+		Vector2d half(0.5, 0.5);
+		Vector2d threequarters(0.75, 0.75);
+		Vector2d tmpa = onehalf - fx;
+		Vector2d tmpb = fx - one;
+		Vector2d tmpc = fx - half;
+		Vector2d w[3] = {
+		  0.5 * (tmpa.cwiseProduct(tmpa)),
+		  threequarters - (tmpb.cwiseProduct(tmpb)),
+		  0.5 * (tmpc.cwiseProduct(tmpc))
+		};
+
+		p.C = Matrix2d::Zero();
+		p.v = Vector2d::Zero();
+
+		// constructing affine per-particle momentum matrix from APIC / MLS-MPM.
+		// see APIC paper (https://www.math.ucla.edu/~jteran/papers/JSSTS15.pdf), page 6
+		// below equation 11 for clarification. this is calculating C = B * (D^-1) for APIC equation 8,
+		// where B is calculated in the inner loop at (D^-1) = 4 is a constant when using quadratic interpolation functions
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				Vector2d dpos = (Vector2d(i, j) - fx);
+				Vector3d curr = grid[base_coord.x() + i][base_coord.y() + j];
+				Vector2d grid_v(curr.x(), curr.y());
+				double weight = w[i].x() * w[j].y();
+				// Velocity
+				p.v += weight * grid_v;
+				// APIC C, outer product of weighted velocity and dist, paper equation 10
+				p.C += 4 * INV_DX * ((weight * grid_v) * dpos.transpose());
+			}
+		}
+
+		// Advection
+		//double tempy = p.x.y();
+		p.x += DT * p.v;
+		//cout << "change" << tempy - p.x.y() << endl;
+		//cout << "velocity " <<  p.v << endl;
+
+		// MLS-MPM F-update eqn 17
+		Matrix2d F = (Matrix2d::Identity() + DT * p.C) * p.F;
+
+		JacobiSVD<Matrix2d> svd(F, ComputeFullU | ComputeFullV);
+		Matrix2d svd_u = svd.matrixU();
+		Matrix2d svd_v = svd.matrixV();
+		Matrix2d sig = svd.singularValues().asDiagonal();
+
+		// Snow Plasticity
+		sig = sig.array().min(1.0f + 7.5e-3).max(1.0 - 2.5e-2);
+
+		double oldJ = F.determinant();
+		F = svd_u * sig * svd_v.transpose();
+
+		double Jp_new = min(max(p.Jp * oldJ / F.determinant(), 0.6), 20.0);
+
+		p.Jp = Jp_new;
+		p.F = F;
 	}
 }
 
 void Update(void)
 {
-	ComputeDensityPressure();
-	ComputeForces();
-	Integrate();
-
+	P2G();
+	UpdateGridVelocity();
+	G2P();
 	glutPostRedisplay();
 }
 
@@ -200,7 +273,7 @@ void InitGL(void)
 {
 	glClearColor(0.9f, 0.9f, 0.9f, 1);
 	glEnable(GL_POINT_SMOOTH);
-	glPointSize(H / 2.f);
+	glPointSize(2);
 	glMatrixMode(GL_PROJECTION);
 }
 
@@ -215,7 +288,7 @@ void Render(void)
 	glColor4f(0.2f, 0.6f, 1.0f, 1);
 	glBegin(GL_POINTS);
 	for (auto& p : particles)
-		glVertex2f(p.x(0), p.x(1));
+		glVertex2f(p.x(0) * VIEW_WIDTH, p.x(1) * VIEW_HEIGHT);
 	glEnd();
 
 	glutSwapBuffers();
@@ -230,11 +303,7 @@ void Keyboard(unsigned char c, __attribute__((unused)) int x, __attribute__((unu
 			std::cout << "maximum number of particles reached" << std::endl;
 		else
 		{
-			unsigned int placed = 0;
-			for (float y = VIEW_HEIGHT / 1.5f - VIEW_HEIGHT / 5.f; y < VIEW_HEIGHT / 1.5f + VIEW_HEIGHT / 5.f; y += H * 0.95f)
-				for (float x = VIEW_WIDTH / 2.f - VIEW_HEIGHT / 5.f; x <= VIEW_WIDTH / 2.f + VIEW_HEIGHT / 5.f; x += H * 0.95f)
-					if (placed++ < BLOCK_PARTICLES && particles.size() < MAX_PARTICLES)
-						particles.push_back(Particle(x, y));
+			addParticles(0.5, 0.5);
 		}
 		break;
 	case 'r':
@@ -249,7 +318,7 @@ int main(int argc, char** argv)
 {
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	glutInit(&argc, argv);
-	glutCreateWindow("Muller SPH");
+	glutCreateWindow("MPM");
 	glutDisplayFunc(Render);
 	glutIdleFunc(Update);
 	glutKeyboardFunc(Keyboard);
